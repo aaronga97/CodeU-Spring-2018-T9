a56e28bafcc0e9e5f6abd8ac4fb64e337beb2a20
your_username <ppbonds@gmail.com>
2018-05-30 16:27:44 -0500

    Fixed activity feed test

--- src/test/java/codeu/model/data/ActivityTest.java
+++ src/test/java/codeu/model/data/ActivityTest.java
@@ -5,23 +5,23 @@ import java.util.UUID;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class ConversationTest {
+public class ActivityTest {
 
   @Test
   public void testCreate() {
     UUID activityId = UUID.randomUUID();
 		Instant creation = Instant.now();
 		String message = "Test_Message";
-		UUID userID = UUID.randomUUID();
+		UUID userId = UUID.randomUUID();
     String username = "Test_Username";
 
     Activity activity = new Activity(activityId, creation, message, userId, username);
 
-    Assert.assertEquals(activityID, activity.getActivityId());
+    Assert.assertEquals(activityId, activity.getActivityId());
 		Assert.assertEquals(0, activity.getAllTimeCount());
     Assert.assertEquals(creation, activity.getCreationTime());
     Assert.assertEquals(message, activity.getMessage());
-    Assert.assertEquals(userID, activity.getUserId());
+    Assert.assertEquals(userId, activity.getUserId());
 		Assert.assertEquals(username, activity.getUsername());
   }
 }
