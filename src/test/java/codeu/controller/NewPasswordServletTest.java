afe5b1255048c28072c3c420ad42f8564508715d
Lee-Roy Gaseller <leeroygaseller@gmail.com>
2018-06-15 14:53:51 -0500

    updated newPasswordServlet tests

--- src/test/java/codeu/controller/NewPasswordServletTest.java
+++ src/test/java/codeu/controller/NewPasswordServletTest.java
@@ -43,6 +43,22 @@ public class NewPasswordServletTest {
     }
     /**test for when a user enters a bad password*/
     @Test
+    public void testDoPost_BadPassword() throws IOException, ServletException {
+        Mockito.when(mockRequest.getParameter("usernameVerification")).thenReturn("test username");
+        Mockito.when(mockRequest.getParameter("newPassword")).thenReturn("badPassword");
+        Mockito.when(mockRequest.getParameter("reTypedNewPassword")).thenReturn("badPassword");
+        UserStore mockUserStore = Mockito.mock(UserStore.class);
+        Mockito.when(mockUserStore.isUserRegistered("test username")).thenReturn(true);
+        newPasswordServlet.setUserStore(mockUserStore);
+
+        newPasswordServlet.doPost(mockRequest, mockResponse);
+
+        Mockito.verify(mockRequest)
+                .setAttribute("error", "Password must be between 5 and 13 characters and contain both letters and numbers.");
+        Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+    }
+
+    @Test
     public void testDoPost_BadShortPassword() throws IOException, ServletException {
         Mockito.when(mockRequest.getParameter("usernameVerification")).thenReturn("test username");
         Mockito.when(mockRequest.getParameter("newPassword")).thenReturn("bad1");
@@ -59,7 +75,7 @@ public class NewPasswordServletTest {
     }
 
     @Test
-    public void testDoPost_BadShortLongPassword() throws IOException, ServletException {
+    public void testDoPost_BadLongPassword() throws IOException, ServletException {
         Mockito.when(mockRequest.getParameter("usernameVerification")).thenReturn("test username");
         Mockito.when(mockRequest.getParameter("newPassword")).thenReturn("badPassword12345");
         Mockito.when(mockRequest.getParameter("reTypedNewPassword")).thenReturn("badPassword12345");
@@ -100,30 +116,19 @@ public class NewPasswordServletTest {
                         Instant.now(),
                         false);
 
-       // Mockito.verify(mockUserStore).addUser(user);
 
         UserStore mockUserStore = Mockito.mock(UserStore.class);
         Mockito.when(mockUserStore.isUserRegistered("test username")).thenReturn(true);
         Mockito.when(mockUserStore.getUser("test username")).thenReturn(user);
-        //Mockito.verify(mockUserStore).addUser(user);
         newPasswordServlet.setUserStore(mockUserStore);
 
         Mockito.when(mockRequest.getParameter("usernameVerification")).thenReturn("test username");
         Mockito.when(mockRequest.getParameter("newPassword")).thenReturn("testpassword1");
         Mockito.when(mockRequest.getParameter("reTypedNewPassword")).thenReturn("testpassword1");
 
-
-
-
         newPasswordServlet.setUserStore(mockUserStore);
 
-
-        String hashedPassword = BCrypt.hashpw("testpassword1", BCrypt.gensalt());
-
-
         newPasswordServlet.doPost(mockRequest, mockResponse);
-
-        ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
 
         Assert.assertEquals("test username", user.getName());
        Assert.assertNotEquals(
