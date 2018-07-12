e9998b3cf954849f2bb8300f9622ebe7a2ece679
your_username <ppbonds@gmail.com>
2018-07-12 05:11:46 -0500

    Toggle sort feature implemented

--- src/main/java/codeu/controller/ActivityFeedServlet.java
+++ src/main/java/codeu/controller/ActivityFeedServlet.java
@@ -3,8 +3,11 @@ package codeu.controller;
 import codeu.model.data.Activity;
 import codeu.model.store.basic.ActivityStore;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.Collections;
+import java.util.Comparator;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -20,7 +23,7 @@ import org.jsoup.safety.Whitelist;
 
 public class ActivityFeedServlet extends HttpServlet {
 
-  /** Store class that gives access to Conversations. */
+  /** Store class that gives access to Activities. */
   private ActivityStore activityStore;
 
   /**
@@ -51,24 +54,61 @@ public class ActivityFeedServlet extends HttpServlet {
     List<Activity> activities = activityStore.getActivities();
 
     request.setAttribute("activities", activities);
+    request.setAttribute("checked", "recent");
 
     request.getRequestDispatcher("/WEB-INF/view/activityfeed.jsp").forward(request, response);
-
 
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-    String username = request.getParameter("searchQuery");
+    List<Activity> activities = (List<Activity>) request.getSession().getAttribute("activities");
 
-    String cleanedUsername = Jsoup.clean(username, Whitelist.none());
 
-    List<Activity> activities = activityStore.getUserActivities(cleanedUsername);
+    /*If the user posts a search request that isn't an empty string, reassembles the list of activities */
+    if (request.getParameter("searchQuery") != null && !request.getParameter("searchQuery").equals("")) {
+      String username = request.getParameter("searchQuery");
+
+      String cleanedUsername = Jsoup.clean(username, Whitelist.none());
+
+      activities = activityStore.getUserActivities(cleanedUsername);
+      request.setAttribute("searchQuery", cleanedUsername);
+    }
+
+    /* If one of the sorting radio buttons are been toggled, sorts the list of activities*/
+
+    if (request.getParameter("sortingStyle") != null) {
+
+      if(request.getParameter("sortingStyle").equals("popular")) {
+
+        /*Sorts activities by allTimeCount field*/
+        Collections.sort(activities, new Comparator<Activity>() {
+          public int compare(Activity one, Activity other) {
+            return other.getAllTimeCount() - one.getAllTimeCount();
+          }
+        });
+
+        request.setAttribute("checked", "popular");
+
+      } else if (request.getParameter("sortingStyle").equals("recent")) {
+
+        /*Sorts activities by creationTime field*/
+        Collections.sort(activities, new Comparator<Activity>() {
+          public int compare(Activity one, Activity other) {
+            return other.getCreationTime().compareTo(one.getCreationTime());
+          }
+        });
+
+        request.setAttribute("checked", "recent");
+
+      }
+
+
+    }
 
     request.setAttribute("activities", activities);
 
     request.getRequestDispatcher("/WEB-INF/view/activityfeed.jsp").forward(request, response);
   }
-
 }
